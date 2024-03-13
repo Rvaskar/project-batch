fcf035cbe019bd93616eaf4a573f4420fc35e357
RutikVaskar <rutikkvaskar9@gmail.com>
2024-03-13 10:32:08 +0530

    last commit 14 march

--- weather/script.js
+++ weather/script.js
@@ -1,20 +1,29 @@
 let input = document.querySelector("input");
 let search = document.querySelector('button')
-let city = document.querySelector('h2')
+let city1 = document.querySelector('h2')
 let temp = document.querySelectorAll('h3')[0]
 let climate = document.querySelectorAll('h3')[1]
 
 // console.log(input,search,city,temp,climate)
 
-async function wheather(){
+async function wheather(city){
     try {
-        let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=10.99&lon=44.34&appid=245d68f2293ab03e260180bf138a0b35`)
+        let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`)
         console.log(data)
         let data1 = data.json()
         data1 = await data1
         console.log(data1)
+        city1.innerText = data1.name
+        temp.innerText = data1.main.temp
+        climate.innerText = data1.weather[0].main
     } catch (error) {
         console.log(error)
     }
 }
-wheather()
+// wheather()
+
+search.addEventListener('click',()=>{
+    let city = input.value;
+
+    wheather(city)
+})
